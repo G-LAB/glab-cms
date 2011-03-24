43c39efe30357113f877b98a08c267e409652183
Ryan Brodkin <ryan@glabstudios.com>
2011-03-23 19:57:28 -0700

    Updated error page to use updated style and script locations.

--- errors/error_general.php
+++ errors/error_general.php
@@ -2,29 +2,31 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
-<title>G LAB (cms) :: <?php echo $heading; ?></title>
-
+<title>G LAB (cms)</title>
 <link rel="stylesheet" type="text/css" href="https://ajax.googleapis.com/ajax/libs/yui/2.8.0r4/build/reset-fonts/reset-fonts.css"/>
-<link rel="stylesheet" type="text/css" href="/styles/jquery/jquery-ui-1.8.1.custom.css" />
-<link rel="stylesheet" type="text/css" href="/styles/global.css"/>
-<link rel="stylesheet" type="text/css" href="/styles/backend.css"/>
-
+<link rel="stylesheet" type="text/css" href="<?=assets_url()?>styles/jquery/jquery-ui-1.7.2.custom.css" />
+<link rel="stylesheet" type="text/css" href="<?=assets_url()?>styles/global.css"/>
+<link rel="stylesheet" type="text/css" href="<?=assets_url()?>styles/backend.css"/>
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+<script type="text/javascript">
+  // Load jQuery
+  google.load("jquery", "1");
+</script>
+<script type="text/javascript" src="<?=assets_url()?>js/jquery-ui-1.7.2.custom.min.js"></script>
 </head>
 
 <body class="minimal">
 
 <div id="hd">
-<div id="suphd">
-	<a href="<?=site_url()?>" id="ScreenBug" title="Go to Dashboard"><span>Go to Dashboard</span></a>
+	<div id="suphd">
+		<a href="<?=site_url('dashboard')?>" id="ScreenBug" title="Go to Dashboard"><span>Go to Dashboard</span></a>
+	</div>
+	<div id="subhd">
+		<h1>G-LAB</h1>
+	</div>
 </div>
-<div id="subhd">
-	<h1>G-LAB</h1>
-</div>
-</div>
-<div id="user">
-</div>
-<div id="bd" class="fancy_corners">
-	<div id="bd-inner">
+<div id="bd">
+	<div id="bd-inner" class="fancy_corners">
 		<h2><?php echo $heading; ?></h2>
 		
 		<div id="target">
@@ -35,5 +37,6 @@
 	</div>
 	<div class="clear"></div>
 </div>
+
 </body>
 </html>
