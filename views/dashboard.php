2130490e794fd9becf50c562a6ae92b596329fbc
Ryan Brodkin <ryan@glabstudios.com>
2011-03-31 11:35:48 -0400

    Added HTML and supporting JavaScript to allow the installation of the G LAB (cms) as a Chrome Browser App.

--- views/dashboard.php
+++ views/dashboard.php
@@ -15,11 +15,19 @@
 		</li>
 		<li class="settings"><a href="<?php echo site_url('admin/settings') ?>">My Settings</a></li>
 	</ul>
+	<div class="clearfix">&nbsp;</div>
 	
 	<?php if ($lastLogin != null && $this->session->userdata('tsCreated')-$lastLogin > 60*60*48) : ?>
-	<div class="msg warning clear">
+	<div class="msg warning clearfix">
 		<strong><?=$this->entity->getValue('firstName')?>, we haven't seen you lately... </strong><br/>
 		Where have you been? It has been <?=strtolower(timespan($lastLogin,$this->session->userdata('tsCreated')))?> since you last signed in. Don't forget to sign in daily!
+	</div>
+	<?php endif; ?>
+	
+	<?php if (strpos($this->input->user_agent(), 'Chrome')) : ?>
+	<div id="chrome_app" class="msg success clearfix hide">
+		<a href="http://glabstudios.com/backend/chrome_app.crx" class="floatr">Install Now</a>
+		Install this system as a Chrome application!
 	</div>
 	<?php endif; ?>
 	
@@ -71,10 +79,15 @@
 		// Tabs
 		$('#box_rss').tabs({ selected: <?=rand(0,count($rss)-1)?> });
 		
+		// Update VM Count
 		$.getJSON('<?=site_url('dashboard/ajax')?>', function(data) {
 		  $('#vmCount').html(data.vmCount + ' New');
 		});
 		
+		// Show Chrome Message, If Applicable
+		if (window.chrome && window.chrome.app && window.chrome.app.isInstalled==false) {
+		  $('#chrome_app').removeClass('hide');
+		}
 
 	});
 </script>
