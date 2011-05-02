3007eec5f90821052cd82bf7f423d40dd81de49b
Ryan Brodkin <ryan@glabstudios.com>
2011-05-02 07:41:31 -0700

    Removed gitweb iframe in favor of link to GitHub.

--- controllers/version_control.php
+++ controllers/version_control.php
@@ -8,9 +8,7 @@ class Version_control extends CI_Controller {
 	
 	function repository() {
 		
-		$projects = array();
-		
-		$console['body'] = $this->load->view('version_control/repository', array('data'=>$projects), TRUE);
+		$console['body'] = $this->load->view('version_control/repository', false, true);
 		
 		$data['content']['body'] = $this->load->view('console', $console, true);
 		$data['content']['side'] = $this->load->view('_sidebar', null, true);
--- views/version_control/repository.php
+++ views/version_control/repository.php
@@ -1,3 +1,12 @@
 <div class="mid body">
-	<iframe src="https://glabstudios.com/git/" width="100%" height="1200" frameborder="0"></iframe>
+	<p>Version control now hosted at</p>
+	<a href="http://github.com/">
+		<img src="http://fi.github.com/images/modules/demo/github_logo.png" alt="GitHub" border="0"/>
+	</a>
+	<p>
+		<br/>
+		<a href="https://github.com/organizations/G-LAB" class="button">
+			Go to G LAB on GitHub
+		</a>
+	</p>
 </div>
