e6fcb2e9148b7b552b0cd1c506fda98664133a3a
Ryan Brodkin <ryan@glabstudios.com>
2011-06-26 08:43:18 -0700

    Cleaned up formatting of domain product views.

--- views/domain_names/contact_set.php
+++ views/domain_names/contact_set.php
@@ -1,6 +1,16 @@
-<strong><?=$first_name.' '.$last_name?></strong><br/>
-<strong><?=$org_name?></strong>
-<address>
-
-</address>
-<?=phone_format($phone)?>
+<span class="vcard">
+	<span class="fn"><strong><?=$first_name.' '.$last_name?></strong></span><br/>
+	<span class="org"><strong><?=$org_name?></strong></span><br/><br/>
+	
+	<span class="email"><?=$email?></span><br/><br/>
+	
+	<address class="adr">
+		<span class="street-address"><?=$address1?><br/></span>
+		<span class="street-address"><?php if ($address2) echo $address2.'<br/>'?></span>
+		<span class="street-address"><?php if ($address3) echo $address3.'<br/>'?></span>
+		<span class="locality"><?=$city?></span>, <span class="region"><?=$state?></span>  <span class="postal-code"><?=$postal_code?></span><br/>
+	</address><br/><br/>
+	
+	<span class="tel"><span class="value"><?=phone_format($phone)?></span></span><br/>
+	<?php if ($fax): ?><span class="tel"><span class="type hide">Fax</span><span class="value"><?=phone_format($fax)?></span></span><br/><?php endif; ?>
+</span>
--- views/domain_names/domain.php
+++ views/domain_names/domain.php
@@ -1,6 +1,6 @@
 <div class="mid header">
 	<h4><?=$domain?></h4>
-	<p>Domain renews <?=date_relative(element('registry_expiredate', $data))?>.</p>
+	<p>Domain <?=(element('auto_renew', $data)) ? 'auto-':''?>renews <?=date_relative(element('registry_expiredate', $data))?>.</p>
 </div>
 <div class="mid body">
 	<table>
@@ -37,15 +37,13 @@
 	<table>
 		<thead>
 			<tr>
-				<td>Nameserver</td>
-				<td>IP Address</td>
+				<td>Nameserver/IP Address</td>
 			</tr>
 		</thead>
 		<tbody>
 			<?php if (element('nameserver_list', $data)) foreach (element('nameserver_list', $data) as $ns) : ?>
 			<tr>
-				<td><?=element('name', $ns)?></td>
-				<td><?=element('ipaddress', $ns)?></td>
+				<td><?=element('name', $ns)?><?=element('ipaddress', $ns)?></td>
 			</tr>
 			<?php endforeach; ?>
 		</tbody>
@@ -55,18 +53,19 @@
 	<h5>Registered Contacts</h5>
 	<?php $contact_set = element('contact_set', $data)?>
 </div>
-<div class="mid body floatl">
+<div class="mid body">
+	<h6>Registered Owner</h6>
+	<?=$this->load->view('domain_names/contact_set', element('owner', $contact_set), TRUE)?>
+</div>
+<div class="mid body">
+	<h6>Administrator</h6>
 	<?=$this->load->view('domain_names/contact_set', element('admin', $contact_set), TRUE)?>
 </div>
-<div class="mid body floatl">
-	Contact
+<div class="mid body">
+	<h6>Billing</h6>
+	<?=$this->load->view('domain_names/contact_set', element('billing', $contact_set), TRUE)?>
 </div>
-<div class="mid body floatl">
-	Contact
+<div class="mid body">
+	<h6>Technical</h6>
+	<?=$this->load->view('domain_names/contact_set', element('tech', $contact_set), TRUE)?>
 </div>
-<div class="mid body floatl">
-	Contact
-</div>
-<pre>
-<?=var_export($data)?>
-</pre>
