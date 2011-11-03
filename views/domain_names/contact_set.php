89ef3bb5501e2a8d52eee8a4c604abfc03122201
Ryan Brodkin <ryan@glabstudios.com>
2011-11-03 08:03:10 -0700

    Enhancing security through stretch match.

--- views/domain_names/contact_set.php
+++ views/domain_names/contact_set.php
@@ -12,5 +12,5 @@
 	</address><br/><br/>
 	
 	<span class="tel"><span class="value"><?=tel_format($phone)?></span></span><br/>
-	<?php if ($fax): ?><span class="tel"><span class="type hide">Fax</span><span class="value"><?=tel_format($fax)?></span></span><br/><?php endif; ?>
+	<?php if (empty($fax) !== true) : ?><span class="tel"><span class="type hide">Fax</span><span class="value"><?=tel_format($fax)?></span></span><br/><?php endif; ?>
 </span>
