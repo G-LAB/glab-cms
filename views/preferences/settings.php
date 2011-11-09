aa9a60e14a33b1a49afc2e83d1348f91b0983cde
Ryan Brodkin <ryan@glabstudios.com>
2011-11-08 20:17:23 -0800

    Adding line breaks to preferences form.

--- views/preferences/settings.php
+++ views/preferences/settings.php
@@ -3,27 +3,27 @@
 	<form action="<?= current_url() ?>" method="post" class="generic">
 		
 		<label>
-			Phone Extension
+			Phone Extension<br/>
 			<input type="text" name="pbx_ext" value="<?=set_value('pbx_ext',$profile->meta->pbx_ext)?>" />
 		</label>
 		
 		<label>
-			Click-to-call Extension/Number
+			Click-to-call Extension/Number<br/>
 			<input type="text" name="pbx_callback" value="<?=set_value('pbx_callback',$profile->meta->pbx_callback)?>" />
 		</label>
 		
 		<label>
-			Voicemail Box
+			Voicemail Box<br/>
 			<input type="text" name="pbx_ext_mbox" value="<?=set_value('pbx_ext_mbox',$profile->meta->pbx_ext_mbox)?>" />
 		</label>
 		
 		<label>
-			Local Timezone
+			Local Timezone<br/>
 			<?=timezone_menu($profile->meta->time_zone)?>
 		</label>
 
 		<label>
-			Time Format
+			Time Format<br/>
 			<?=form_dropdown('time_format',array('12-Hour Time ('.date('g:i A').')','24-Hour Time ('.date('H:i').')'),set_value('time_format',$profile->meta->time_format))?>
 		</label>
 		
