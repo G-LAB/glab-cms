ffa89fc13bc330c21f68ca0700f491a9aa149d2a
Ryan Brodkin <ryan@glabstudios.com>
2011-12-05 21:14:01 -0800

    Switched Communication controller to Template library.

--- controllers/communication.php
+++ controllers/communication.php
@@ -13,12 +13,6 @@ class Communication extends CI_Controller
         $this->load->model('ticket');
         $this->load->helper('glib_file');
         $this->load->helper('text');
-        
-        $this->cmenu[] = array('url'=>'communication/tickets', 'text'=>'Support Tickets', 'attr'=>'class="ticket"', 'count'=>$this->ticket->get_count());
-        //$this->cmenu[] = array('url'=>'knowledge_base', 'text'=>'Knowledge Base', 'attr'=>'class="kb"');
-        $this->db->where('is_new',TRUE);
-        $this->cmenu[] = array('url'=>'communication/fax_messages', 'text'=>'Fax Messages', 'attr'=>'class="fax"', 'count'=>$this->document->get_count_new());
-        //$this->cmenu[] = array('url'=>'communication/announcements', 'text'=>'Announcements', 'attr'=>'class="annc"');
 	}
 	
 	function index () 
@@ -52,18 +46,9 @@ class Communication extends CI_Controller
 		$config['per_page'] = '5';
 		$this->pagination->initialize($config); 
 		
-		$console['header'] = 'Showing '.($offset + 1).' - '.($offset + $count_this).' of '.$count_total;
-		
-		$console['body'] = $this->load->view('documents/list', array('data'=>$docs), TRUE);
-		
-		$console['footer_lt'] = $this->pagination->create_links();
-		$console['footer_rt'] = $console['header'];
-		
-		$data['content']['body'] = $this->load->view('console', $console, true);
-		$data['content']['side'] = $this->load->view('_sidebar', null, true);
-		
-		$this->load->view('main',$data);
-		
+		//$console['header'] = 'Showing '.($offset + 1).' - '.($offset + $count_this).' of '.$count_total;
+		//$console['footer_lt'] = $this->pagination->create_links();
+		$this->template->build('documents/list', array('data'=>$docs));
 	}
 	
 	function tickets ($qid=null) 
@@ -78,32 +63,12 @@ class Communication extends CI_Controller
 		$tickets = $this->ticket->get_tickets($qid, $status);
 		
 		// Average Time to Resolution
-		$responseTimeResolution = $this->db->query("SELECT (AVG(diff)/60/60) as avg FROM (SELECT unix_timestamp(timestamp)-unix_timestamp((SELECT timestamp FROM com_entry e2 LEFT JOIN event_log l ON e2.event = l.evid WHERE e2.tiknum = dt1.tiknum LIMIT 1)) as diff FROM (SELECT tiknum, timestamp FROM com_entry e1 LEFT JOIN event_log l ON e1.event = l.evid WHERE status = 'closed' AND l.timestamp > DATE_SUB(CURRENT_DATE, INTERVAL 30 DAY) ORDER BY e1.enid DESC) as dt1 GROUP BY tiknum) as dt2")->row();
+		/*$responseTimeResolution = $this->db->query("SELECT (AVG(diff)/60/60) as avg FROM (SELECT unix_timestamp(timestamp)-unix_timestamp((SELECT timestamp FROM com_entry e2 LEFT JOIN event_log l ON e2.event = l.evid WHERE e2.tiknum = dt1.tiknum LIMIT 1)) as diff FROM (SELECT tiknum, timestamp FROM com_entry e1 LEFT JOIN event_log l ON e1.event = l.evid WHERE status = 'closed' AND l.timestamp > DATE_SUB(CURRENT_DATE, INTERVAL 30 DAY) ORDER BY e1.enid DESC) as dt1 GROUP BY tiknum) as dt2")->row();
 		$sideData['resolution'] = round($responseTimeResolution->avg);
-		$side['subMenu'] = $this->load->view('communication/ticket_side', $sideData, true);
+		$side['subMenu'] = $this->load->view('communication/ticket_side', $sideData, true);*/
 		
-		$console['header'] = count($tickets).' Tickets<br/>';
-	
-		$console['body'] = $this->load->view('communication/ticket_list', array('tickets'=>$tickets), TRUE);
-		
-		$console['footer_lt'] = null;
-		$console['footer_rt'] = $console['header'];
-		
-		if (is_numeric($qid) === true AND $qid != 0) 
-		{
-			$queue = $this->ticket->get_queue($qid);
-			$data['content']['nav']['title'] = $queue['name'];
-		}
-		else 
-		{
-			$data['content']['nav']['title'] = 'All Tickets';
-		}
-		
-		$data['content']['body'] = $this->load->view('console', $console, true);
-		$data['content']['side'] = $this->load->view('_sidebar', $side, true);
-		
-		$this->load->view('main',$data);	
-	
+		//$console['header'] = count($tickets).' Tickets<br/>';
+		$this->template->build('communication/ticket_list', array('tickets'=>$tickets));
 	}
 
 	function ticket_view ($tikid=null) 
@@ -122,22 +87,13 @@ class Communication extends CI_Controller
 		}
 		
 		// Average Time to Resolution
-		$responseTimeResolution = $this->db->query("SELECT (AVG(diff)/60/60) as avg FROM (SELECT unix_timestamp(timestamp)-unix_timestamp((SELECT timestamp FROM com_entry e2 LEFT JOIN event_log l ON e2.event = l.evid WHERE e2.tiknum = dt1.tiknum LIMIT 1)) as diff FROM (SELECT tiknum, timestamp FROM com_entry e1 LEFT JOIN event_log l ON e1.event = l.evid WHERE status = 'closed' AND l.timestamp > DATE_SUB(CURRENT_DATE, INTERVAL 30 DAY) ORDER BY e1.enid DESC) as dt1 GROUP BY tiknum) as dt2")->row();
+		/*$responseTimeResolution = $this->db->query("SELECT (AVG(diff)/60/60) as avg FROM (SELECT unix_timestamp(timestamp)-unix_timestamp((SELECT timestamp FROM com_entry e2 LEFT JOIN event_log l ON e2.event = l.evid WHERE e2.tiknum = dt1.tiknum LIMIT 1)) as diff FROM (SELECT tiknum, timestamp FROM com_entry e1 LEFT JOIN event_log l ON e1.event = l.evid WHERE status = 'closed' AND l.timestamp > DATE_SUB(CURRENT_DATE, INTERVAL 30 DAY) ORDER BY e1.enid DESC) as dt1 GROUP BY tiknum) as dt2")->row();
 		$sideData['resolution'] = round($responseTimeResolution->avg);
-		$side['subMenu'] = $this->load->view('communication/ticket_side', $sideData, true);
+		$side['subMenu'] = $this->load->view('communication/ticket_side', $sideData, true);*/
 		
-		$console['header'] = '<strong>Ticket ID:</strong> '.$tikid.'<br/>'.count($entries).' Messages';
-		$console['body'] = $this->load->view('communication/ticket_view', array('ticket'=>$this->ticket->get_ticket($tiknum), 'entries'=>$entries), TRUE);
-		$console['footer_lt'] = null;
-		$console['footer_rt'] = count($entries).' Messages';
-		
-		$data['content']['nav']['title'] = $tikid;
-		
-		$data['content']['body'] = $this->load->view('console', $console, true);
-		$data['content']['side'] = $this->load->view('_sidebar', null, true);
-		
-		$this->load->view('main',$data);	
-	
+		//$console['header'] = '<strong>Ticket ID:</strong> '.$tikid.'<br/>'.count($entries).' Messages';
+		//$console['footer_rt'] = count($entries).' Messages';
+		$this->template->build('communication/ticket_view', array('ticket'=>$this->ticket->get_ticket($tiknum), 'entries'=>$entries));
 	}
 	
 	function ticket_reply () 
